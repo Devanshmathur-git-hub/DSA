94cf26eb382eb6ae84cdb805941babfffc5c3df3
DEVANSH MATHUR <186853379+Devanshmathur-git-hub@users.noreply.github.com>
2026-07-02 15:00:30 +0530

    Time: 0 ms (100%), Space: 15.8 MB (35.59%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -4,25 +4,22 @@ public:
 
         int n = nums.size();
 
+        // Step 1: Find the first decreasing element from the right
         int i = n - 2;
-
-        // Step 1: Find pivot
         while (i >= 0 && nums[i] >= nums[i + 1]) {
             i--;
         }
 
-        // Step 2: Find next greater element
+        // Step 2: Find the next greater element and swap
         if (i >= 0) {
             int j = n - 1;
-
             while (nums[j] <= nums[i]) {
                 j--;
             }
-
             swap(nums[i], nums[j]);
         }
 
-        // Step 3: Reverse remaining part
+        // Step 3: Reverse the suffix
         reverse(nums.begin() + i + 1, nums.end());
     }
 };
