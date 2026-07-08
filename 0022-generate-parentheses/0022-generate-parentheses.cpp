daf3600b86e7b3df835f7b08cb6f07001c317ec1
DEVANSH MATHUR <186853379+Devanshmathur-git-hub@users.noreply.github.com>
2026-07-08 14:43:08 +0530

    Time: 3 ms (70.34%), Space: 15.7 MB (41.26%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
-    void solve(int open, int close, int n, string s, vector<string>& ans)
-    {
-        // Base Case
+    void solve(int open, int close, int n, string s, vector<string>& ans){
+    
         if (s.length() == 2 * n)
         {
             ans.push_back(s);
             return;
+            
         }
-
-        // Add '('
-        if (open < n)
+        if (open < n)              // Add '('
         {
             solve(open + 1, close, n, s + "(", ans);
         }
 
-        // Add ')'
-        if (close < open)
+       
+        if (close < open)          // Add ')'
         {
             solve(open, close + 1, n, s + ")", ans);
         }
@@ -25,7 +23,6 @@ public:
     vector<string> generateParenthesis(int n)
     {
         vector<string> ans;
-
         solve(0, 0, n, "", ans);
 
         return ans;
