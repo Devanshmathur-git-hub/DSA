9247ddbe4265d970d80629efbb8317cd1a41d17f
DEVANSH MATHUR <186853379+Devanshmathur-git-hub@users.noreply.github.com>
2026-07-16 14:46:36 +0530

    Time: 84 ms (41.53%), Space: 88.9 MB (49.57%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -1,60 +1,56 @@
-class Solution {
-public:
-    int longestConsecutive(vector<int>& nums) {
-        unordered_set<int> st(nums.begin(), nums.end());
-
-        int longest = 0;
-
-        for (int num : st) {
-            // Start only if num is the first element of a sequence
-            if (st.find(num - 1) == st.end()) {
-                int curr = num;
-                int len = 1;
-
-                while (st.find(curr + 1) != st.end()) {
-                    curr++;
-                    len++;
-                }
-
-                longest = max(longest, len);
-            }
-        }
-
-        return longest;
-    }
-};
-
-
 // class Solution {
 // public:
 //     int longestConsecutive(vector<int>& nums) {
+//         unordered_set<int> st(nums.begin(), nums.end());
 
-//         unordered_map<int, bool> mp;
+//         int longest = 0;
 
-//         for (int num : nums) {
-//             mp[num] = false;
-//         }
-//         for (auto &it : mp) {
-//             int key = it.first;
+//         for (int num : st) {
+//             // Start only if num is the first element of a sequence
+//             if (st.find(num - 1) == st.end()) {
+//                 int curr = num;
+//                 int len = 1;
 
-//             if (mp.find(key - 1) == mp.end()) {
-//                 mp[key] = true;
-//             }
-//         }
-
-//         int ans = 0;
-       
-//         for (auto &it : mp) {
-//             int key = it.first;
-//             if (mp[key]) {
-//                 int k = 1;
-//                 while (mp.find(key + k) != mp.end()) {
-//                     k++;
+//                 while (st.find(curr + 1) != st.end()) {
+//                     curr++;
+//                     len++;
 //                 }
-//                 ans = max(ans, k);
+
+//                 longest = max(longest, len);
 //             }
 //         }
 
-//         return ans;
+//         return longest;
 //     }
 // };
+
+
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+
+        unordered_map<int, bool> mp;
+        for (int num : nums) {
+            mp[num] = false;
+        }
+        for (auto &it : mp) {
+            int key = it.first;
+            if (mp.find(key - 1) == mp.end()) {
+                mp[key] = true;
+            }
+        }
+
+        int ans = 0;
+        for (auto &it : mp) {
+            int key = it.first;
+            if (mp[key]) {
+                int k = 1;
+                while (mp.find(key + k) != mp.end()) {
+                    k++;
+                }
+                ans = max(ans, k);
+            }
+        }
+        return ans;
+    }
+};
