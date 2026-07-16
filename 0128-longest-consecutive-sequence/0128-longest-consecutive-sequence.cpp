0e414aa2ce5e87bee68696ef6668d8c64ee7d152
DEVANSH MATHUR <186853379+Devanshmathur-git-hub@users.noreply.github.com>
2026-07-16 14:37:06 +0530

    Time: 92 ms (19.55%), Space: 88.9 MB (67.29%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -23,3 +23,38 @@ public:
         return longest;
     }
 };
+
+
+// class Solution {
+// public:
+//     int longestConsecutive(vector<int>& nums) {
+
+//         unordered_map<int, bool> mp;
+
+//         for (int num : nums) {
+//             mp[num] = false;
+//         }
+//         for (auto &it : mp) {
+//             int key = it.first;
+
+//             if (mp.find(key - 1) == mp.end()) {
+//                 mp[key] = true;
+//             }
+//         }
+
+//         int ans = 0;
+       
+//         for (auto &it : mp) {
+//             int key = it.first;
+//             if (mp[key]) {
+//                 int k = 1;
+//                 while (mp.find(key + k) != mp.end()) {
+//                     k++;
+//                 }
+//                 ans = max(ans, k);
+//             }
+//         }
+
+//         return ans;
+//     }
+// };
