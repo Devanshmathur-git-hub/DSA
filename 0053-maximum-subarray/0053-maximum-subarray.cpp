766500689af47f7f89682195e0845e514ef29586
DEVANSH MATHUR <186853379+Devanshmathur-git-hub@users.noreply.github.com>
2026-08-27 01:16:38 +0530

    Time: 0 ms (100%), Space: 71.7 MB (81.36%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -0,0 +1,62 @@
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int current_sum = nums[0];
+        int max_sum = nums[0];
+        for (int i = 1; i < nums.size(); i++) {
+            current_sum = max(nums[i], current_sum + nums[i]);
+            max_sum = max(max_sum, current_sum);
+        }
+        return max_sum;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// //class Solution {
+// //public:
+// //    int maxSubArray(vector<int>& nums) {
+// //
+//   //      int sum = 0;
+//     //    int maxi = nums[0];
+// //
+//   //      for(int num : nums) {
+//     //        sum += num;
+//       //      maxi = max(maxi, sum);
+//         //    if(sum < 0)
+//           //      sum = 0;
+//        // }
+
+//  //       return 0;
+// //    }
+// // };
+
+// //kadanse algo
+
+// class Solution {
+// public:
+//     int maxSubArray(vector<int>& nums) {
+//         int current_sum = nums[0];
+//         int max_sum = nums[0];
+//         for (int i = 1; i < nums.size(); i++) {
+//             current_sum = max(nums[i], current_sum + nums[i]);
+//             max_sum = max(max_sum, current_sum);
+//         }
+//         return max_sum;
+//     }
+// };
+
+// 1752,33,second,81,209,cont acunrand anagram
