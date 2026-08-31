a8de6b75d6c44d7ffcca380dd4e5cc07546b1261
DEVANSH MATHUR <186853379+Devanshmathur-git-hub@users.noreply.github.com>
2026-08-31 11:14:11 +0530

    Time: 4 ms (19.34%), Space: 28.2 MB (44.58%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,21 +1,49 @@
+// class Solution {
+// public:
+//     int majorityElement(vector<int>& nums) {
+//         int candidate = 0;
+//         int count = 0;
+
+//         for (int num : nums) {
+//             if (count == 0) {
+//                 candidate = num;
+//             }
+
+//             if (num == candidate) {
+//                 count++;
+//             } else {
+//                 count--;
+//             }
+//         }
+
+//         return candidate;
+//     }
+// };
+
+
+
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int candidate = 0;
-        int count = 0;
 
-        for (int num : nums) {
-            if (count == 0) {
-                candidate = num;
+        sort(nums.begin(), nums.end());   //sort
+
+        int freq = 1;
+        int ans = nums[0];
+
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] == nums[i - 1]) {
+                freq++;
+            } else {
+                freq = 1;
+                ans = nums[i];
             }
 
-            if (num == candidate) {
-                count++;
-            } else {
-                count--;
+            if (freq > nums.size() / 2) {
+                return ans;
             }
         }
 
-        return candidate;
+        return ans;
     }
 };
