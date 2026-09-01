70580552d9e3393321dab71384c350b5ee1ed3cc
DEVANSH MATHUR <186853379+Devanshmathur-git-hub@users.noreply.github.com>
2026-09-01 12:02:54 +0530

    Time: 1 ms (35.19%), Space: 97.4 MB (25.9%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,14 +1,54 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int minPrice = INT_MAX;
-        int maxProfit = 0;
+        int maxprofit = 0;
+        int besttimetobuy = prices[0];
+        // int n = sizeof(prices);   //array
+        int n = prices.size();        //vector
 
-        for (int price : prices) {
-            minPrice = min(minPrice, price);
-            maxProfit = max(maxProfit, price - minPrice);
+        for(int i=1 ; i<n ; i++){
+            if(prices[i] > besttimetobuy){
+              maxprofit = max(maxprofit , (prices[i] - besttimetobuy));
+            }
+            besttimetobuy = min(besttimetobuy, prices[i]);
         }
-
-        return maxProfit;
+        return maxprofit;
     }
-};
+    };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+// public:
+//     int maxProfit(vector<int>& prices) {
+//         int minPrice = INT_MAX;
+//         int maxProfit = 0;
+
+//         for (int price : prices) {
+//             minPrice = min(minPrice, price);
+//             maxProfit = max(maxProfit, price - minPrice);
+//         }
+
+//         return maxProfit;
+//     }
+// };
